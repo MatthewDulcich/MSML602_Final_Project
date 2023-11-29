0c7224b5fd82f17d60d2dceb6333cfb6aa34a0e7
MatthewDulcich <61712797+MatthewDulcich@users.noreply.github.com>
2023-11-29 03:33:50 -0500

    made a better webpage

--- api/webpage/script.js
+++ api/webpage/script.js
@@ -1,2 +1,18 @@
 // script.js
 console.log("Hello from script.js! This script is running.");
+
+function showDataView() {
+    // Get the main content area
+    const mainContent = document.querySelector('.main-content');
+
+    // Set or load the data view content into the main content area
+    mainContent.innerHTML = '<h2>Data View</h2><p>This is the data view content.</p>';
+}
+
+function showAnalysisView() {
+    // Get the main content area
+    const mainContent = document.querySelector('.main-content');
+
+    // Set or load the analysis view content into the main content area
+    mainContent.innerHTML = '<h2>Analysis View</h2><p>This is the analysis view content.</p>';
+}

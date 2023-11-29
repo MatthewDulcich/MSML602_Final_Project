732371fe364b3b2dfdf2eb94d91fcc171816ddbf
MatthewDulcich <61712797+MatthewDulcich@users.noreply.github.com>
2023-11-28 23:51:11 -0500

    Added app.py and utilities for flask web server

---
+++ api/utilities.py
@@ -0,0 +1,9 @@
+#preprocessing and pipeline and load pickled file
+
+# Test function
+def predict_pipeline(sample):
+    return {'text': 'test text', 'pred': int(0), 'label': 'cat?'}
+
+
+if __name__ == "__main__":
+     print(predict_pipeline("hi"))

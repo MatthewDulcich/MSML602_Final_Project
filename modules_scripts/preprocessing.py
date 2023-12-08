3ea64255902411272faf82a31380c8058bb21ebd
MatthewDulcich <61712797+MatthewDulcich@users.noreply.github.com>
2023-12-08 00:02:54 -0500

    upgraded python version

---
+++ modules_scripts/preprocessing.py
@@ -0,0 +1,37 @@
+# echo "John, 30, New York" >> sample.csv
+# echo "Alice, 25, San Francisco" >> sample.csv
+
+import os
+import csv
+
+def main():
+    folder_name = "../data"
+
+    # Create a folder if it doesn't exist
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+    file_path = os.path.join(folder_name, "data.csv")
+
+    data = [
+        {"Name": "Alice", "Age": 25},
+        {"Name": "Bob", "Age": 30},
+        {"Name": "Charlie", "Age": 35}
+    ]
+
+    # Writing data to CSV file
+    with open(file_path, mode='w', newline='') as file:
+        fieldnames = ["Name", "Age"]
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+        # Write header
+        writer.writeheader()
+
+        # Write data
+        for person in data:
+            writer.writerow(person)
+
+    print(f"Data saved to {file_path}")
+
+if __name__ == "__main__":
+    main()

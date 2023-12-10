3a260bdc1fe0e9e079ed0535e581d810954c85cd
MatthewDulcich <61712797+MatthewDulcich@users.noreply.github.com>
2023-12-10 03:35:02 -0500

    fixed web_scraper.py duplicate file save

--- api/modules_scripts/web_scraper.py
+++ api/modules_scripts/web_scraper.py
@@ -6,32 +6,7 @@ import os
 from secret_key import API_KEY
 # from google.colab import files
 
-data_filepath = '../data'
-params = {
-    'api_key': API_KEY,  # Replace with your actual API key
-    'engine': 'google_play',    # SerpApi search engine
-    'store': 'apps'             # Google Play Apps
-}
- 
-search = GoogleSearch(params)
-result_dict = search.get_dict()
- 
-# Print the entire response to analyze its structure
-print(json.dumps(result_dict, indent=2, ensure_ascii=False))
- 
-# Check for the presence of 'organic_results' key
-if 'organic_results' in result_dict:
-    google_play_apps = result_dict['organic_results']
-    print(json.dumps(google_play_apps, indent=2, ensure_ascii=False))
-else:
-    print("No organic results found in the response.")
- 
 def scrape_apps_from_all_pages(params):
-    # params = {
-    #     'api_key': API_KEY,  # Replace with your actual API key
-    #     'engine': 'google_play',    # SerpApi search engine
-    #     'store': 'apps'             # Google Play Apps
-    # }
  
     search = GoogleSearch(params)
     apps = []
@@ -59,39 +34,52 @@ def scrape_apps_from_all_pages(params):
             break
  
     return apps
- 
-# Get the list of apps
-apps_data = scrape_apps_from_all_pages(params)
-key_vs_value = dict()
-for record in apps_data:
-    # if(record not in set) :
-    #     set.add(record)
-    if(record['title'] not in key_vs_value):
-        key_vs_value[record['title']] = record
- 
 
-apps_data = []
- 
-# for val in apps_data:
- 
-for key in key_vs_value:
-    apps_data.append(key_vs_value.get(key))
- 
-# Convert the list of dictionaries into a pandas DataFrame
-df = pd.json_normalize(apps_data)
- 
-# Save the DataFrame to a CSV file
-csv_filename = 'google_play_apps.csv'
-csv_file = df.to_csv(csv_filename, index=False)
- 
-# Download the CSV file to your local system
-# Check if the folder exists, if not, create it
-if not os.path.exists(data_filepath):
-    os.makedirs(data_filepath)
+if __name__ == "__main__":
+    data_filepath = '../data'
+    params = {
+        'api_key': API_KEY,  # Replace with your actual API key
+        'engine': 'google_play',    # SerpApi search engine
+        'store': 'apps'             # Google Play Apps
+    }
+    
+    search = GoogleSearch(params)
+    result_dict = search.get_dict()
+    
+    # Print the entire response to analyze its structure
+    # print(json.dumps(result_dict, indent=2, ensure_ascii=False))
+    
+    # Check for the presence of 'organic_results' key
+    if 'organic_results' in result_dict:
+        google_play_apps = result_dict['organic_results']
+        # print(json.dumps(google_play_apps, indent=2, ensure_ascii=False))
+    else:
+        print("No organic results found in the response.")
+    
 
-# Define the file path within the data folder
-file_path = os.path.join(data_filepath, csv_filename)
+    # Get the list of apps
+    apps_data = scrape_apps_from_all_pages(params)
+    key_vs_value = dict()
+    for record in apps_data:
+        # if(record not in set) :
+        #     set.add(record)
+        if(record['title'] not in key_vs_value):
+            key_vs_value[record['title']] = record
+    
 
-# Write data to the file
-with open(file_path, 'w') as file:
-    file.write(csv_file)
+    apps_data = []
+    
+    # for val in apps_data:
+    
+    for key in key_vs_value:
+        apps_data.append(key_vs_value.get(key))
+    
+    # Convert the list of dictionaries into a pandas DataFrame
+    df = pd.json_normalize(apps_data)
+    
+    # Save the DataFrame to a CSV file
+    csv_filename = 'google_play_apps.csv'
+    # Define the file path within the data folder
+    file_path = os.path.join(data_filepath, csv_filename)
+
+    df.to_csv(file_path, index=False)
